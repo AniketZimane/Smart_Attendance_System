a792e0596f8d97f61694b332039eb1a69aa885c6
Aniket <ad.developer1604@gmail.com>
2023-04-24 12:14:29 +0530

    data updation process

--- src/main/java/com/example/Smart_Attendance_System/Dao/CourseRepo.java
+++ src/main/java/com/example/Smart_Attendance_System/Dao/CourseRepo.java
@@ -7,8 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface CourseRepo extends JpaRepository<Course,Integer> {
     @Query("select name from Course where courseId = :courseId")
     String getNameByCourseId(Integer courseId);
+
+    List<Course> findByDepartmentId(Integer departmentId);
 }
